5a0b5d256fba02504bb14d7cd61ce05666b74419
Everton Dutra <evertondutra02@gmail.com>
2021-08-15 23:48:04 -0300

    teste 2, valiodando a quantidade de itens no carrinho

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -14,22 +14,37 @@ public class HomePage {
 	
 	List<WebElement> listaProdutos = new ArrayList();
 	
-	// dando nome e apontando para o elemento
+// dando nome e apontando para o elemento
+	private By textoProdutosNoCarrinho = By.className("cart-products-count");
+	
 	private By produtos = By.className("product-description");
 	
 	public HomePage(WebDriver driver) {
 		this.driver = driver;
 	}
-	
+// ##### PARA O TEST 1 #####
 	// aqui vai pegar a lista de produtos e contar
 	public int contarProdutos() {
 		carregarListaprodutos();
 		return listaProdutos.size();
 	}
 	
-	// aqui vai carregar a lista
+// aqui vai carregar a lista
 	private void carregarListaprodutos() {
 		listaProdutos = driver.findElements(produtos);
 	}
+
+//##### PARAOP TEST 2 #####
+	public int obterQuantidadeProdutosNoCarrinho() {
+		String quantidadeProdutosNoCarrinho = driver.findElement(textoProdutosNoCarrinho).getText();
+// retirando os parenteses
+		quantidadeProdutosNoCarrinho = quantidadeProdutosNoCarrinho.replace("(", "");
+		quantidadeProdutosNoCarrinho = quantidadeProdutosNoCarrinho.replace(")", "");
+		
+		int qtdProdutosNoCarrinho = Integer.parseInt(quantidadeProdutosNoCarrinho);
+		
+		return qtdProdutosNoCarrinho;
+	}
+	
 
 }
--- src/test/java/homepage/HomePageTests.java
+++ src/test/java/homepage/HomePageTests.java
@@ -9,12 +9,20 @@ import base.BaseTests;
 
 public class HomePageTests extends BaseTests {
 	// o que vou fazer para contar os produtos
-	@Test
+	
+	@Test  // validar a quantidade de produtos na página
 	public void testContarProdutos_oitoProdutosDiferentes() {
 		carregarPaginaInicial();
 		// no objeto homePage vou acessar o metodo contar produtos
 		//fazendo um assert para validação
 		assertThat(homePage.contarProdutos(), is (8));
+	}
+	
+	
+	@Test  // validar se o carrinho esta vazio
+	public void testValidarCarrinhoZerado_ZeroItensNoCarrinho() {
+		int produtosNoCarrinho = homePage.obterQuantidadeProdutosNoCarrinho();
+		assertThat(produtosNoCarrinho, is(0));
 	}
 
 }
