0eaf04d23bc800cf6ecf3eb113abd5f522347582
Everton Dutra <evertondutra02@gmail.com>
2021-08-16 14:39:51 -0300

    selecionando o produto e alterando a quantidade, cor e tamanho

--- src/main/java/pages/ProdutoPage.java
+++ src/main/java/pages/ProdutoPage.java
@@ -1,7 +1,12 @@
 package pages;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class ProdutoPage {
 
@@ -10,7 +15,11 @@ public class ProdutoPage {
 	// criando o apontamento
 	private By nomeProduto = By.className("h1");
 	private By precoProduto = By.cssSelector(".current-price  span:nth-child(1)");
-
+	private By tamanhoProduto = By.id("group_1");
+	private By inputCorPreta = By.xpath("//ul[@id='group_2']//input[@value='11']");
+	private By quantidadeProduto = By.id("quantity_wanted");
+	private By botaoAddToCard = By.className("add-to-cart");
+	
 	
 	// criando o construtor
 	public ProdutoPage(WebDriver driver) {
@@ -24,6 +33,39 @@ public class ProdutoPage {
 	
 	public String obterPrecoProduto() {
 		return driver.findElement(precoProduto).getText();
+	}
+	// selecionar uma opção do dropdown
+	public void selecionaOpcaoDropDown(String opcao) {
+		encontrarDropdownSize().selectByVisibleText(opcao);
+	}
+	
+	// retornar uma lista stringa do seletor
+	public List<String> obterOpcoesSelecionadas() {
+		List<WebElement> elementosSelecionados =
+		encontrarDropdownSize().getAllSelectedOptions();
+		List<String> listaOpcoesSelecionadas = new ArrayList();
+		for (WebElement elemento : elementosSelecionados) {
+			listaOpcoesSelecionadas.add(elemento.getText());
+		}
+		return listaOpcoesSelecionadas;
+	}
+	//selecionar o tamanho do Produto
+	public Select encontrarDropdownSize() {
+		return new Select(driver.findElement(tamanhoProduto));
+	}
+	//selecionar a cor
+	public void selecionarCorPreta() {
+		driver.findElement(inputCorPreta).click();
+	}
+	//selecionar a quantidade
+	public void alterarQuantidade(int quantidade) {
+		driver.findElement(quantidadeProduto).clear();
+		driver.findElement(quantidadeProduto).sendKeys(Integer.toString(quantidade));;
+	}
+	
+	//clicar no botão add to card
+	public void clicarBotaoaddToCard( ) {
+		driver.findElement(botaoAddToCard).click();
 	}
 
 }
--- src/test/java/homepage/HomePageTests.java
+++ src/test/java/homepage/HomePageTests.java
@@ -3,6 +3,8 @@ package homepage;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import base.BaseTests;
@@ -20,13 +22,13 @@ public class HomePageTests extends BaseTests {
 		assertThat(homePage.contarProdutos(), is (8));
 	}
 	
-	
 	@Test  // validar se o carrinho esta vazio
 	public void testValidarCarrinhoZerado_ZeroItensNoCarrinho() {
 		int produtosNoCarrinho = homePage.obterQuantidadeProdutosNoCarrinho();
 		assertThat(produtosNoCarrinho, is(0));
 	}
 	
+	ProdutoPage produtoPage;
 	@Test  // escolher o item e o preço e valida-los na página do item
 	public void testValidarDetalhesDoProduto_DesrcricaoEValorIguais() {
 		int indice = 0;
@@ -36,23 +38,20 @@ public class HomePageTests extends BaseTests {
 		System.out.println(nomeProduto_HomePage);
 		System.out.println(precoProduto_HomePage);
 		
-		ProdutoPage produtoPage = homePage.clicarProduto(indice);
+		produtoPage = homePage.clicarProduto(indice);
 		
 		String nomeProduto_ProdutoPage = produtoPage.obterNomeProduto();
 		String precoProduto_ProdutoPage = produtoPage.obterPrecoProduto();
-		
-		System.out.println(nomeProduto_ProdutoPage);
-		System.out.println(precoProduto_ProdutoPage);
 		
 		assertThat(nomeProduto_HomePage.toUpperCase(), is (nomeProduto_ProdutoPage.toUpperCase()));
 		assertThat(precoProduto_HomePage, is (precoProduto_ProdutoPage));
 	}
 	
-	
+	LoginPage loginPage;
 	@Test  // Preenchendo o login e validando o usuário logado
 	public void testLoginComSucesso_UsuarioLogado( ) {
 		// Clicar no botão Sign In na home page
-		LoginPage loginPage = homePage.clicarBotaoSignIn();
+		loginPage = homePage.clicarBotaoSignIn();
 		
 		// Preencher usuário e senha
 		loginPage.preencherEmail("evertonteste@teste.com");
@@ -61,7 +60,42 @@ public class HomePageTests extends BaseTests {
 		loginPage.clicarBotaoSignIn();
 		// Validar se o usuário esta logado de fato
 		assertThat(homePage.estaLogado("Everton Teste"), is (true));
+		//Voltar para a página inicial
+		carregarPaginaInicial();
+		
 		
 	}
 
+	@Test
+	public void incluirProdutoNoCarrinho_ProdutoIncluidoComSucesso() {
+		//--Pré condição
+		//verificar se esta logado, se não estiver irá logar
+		if(!homePage.estaLogado("Everton Teste")) {
+			testLoginComSucesso_UsuarioLogado();
+		}
+		// -- Teste
+		// escolher o item e o preço e valida-los na página do item
+		testValidarDetalhesDoProduto_DesrcricaoEValorIguais();
+		
+		//Selecionar Tamanho
+		List<String> listaOpcoes = produtoPage.obterOpcoesSelecionadas();
+		System.out.println(listaOpcoes.get(0));
+		System.out.println("tamanho da lista " + listaOpcoes.size());
+		// * Alterar o tamanho para M
+		produtoPage.selecionaOpcaoDropDown("M");
+		
+		listaOpcoes = produtoPage.obterOpcoesSelecionadas();
+		System.out.println(listaOpcoes.get(0));
+		System.out.println("tamanho da lista " + listaOpcoes.size());
+		
+		// Selecionar Cor
+		produtoPage.selecionarCorPreta();
+		
+		//Selecionar Quantidade
+		produtoPage.alterarQuantidade(2);
+		
+		//Adicionar no carrinho
+		produtoPage.clicarBotaoaddToCard();
+	}
+	
 }
